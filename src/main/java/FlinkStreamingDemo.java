ba40c910345013b93f4b1afc074f96c013455701
Unknown <jasonlai1218@gmail.com>
2017-09-10 16:26:42 +0800

    [Flink] add TransformDigits Class Operator
    add TransformDigits Class Operator

--- src/main/java/FlinkStreamingDemo.java
+++ src/main/java/FlinkStreamingDemo.java
@@ -1,3 +1,4 @@
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
@@ -8,19 +9,33 @@ public class FlinkStreamingDemo {
 
     public static void main(String[] args) throws Exception {
 
-        // flink會自己去判斷自己在什麼環境，目前在IDE中
+        // flink會自己去判斷自己在什麼環境，目前在ide中
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         //data source
-        DataStream<String> elements = env.fromElements("One", "two" , "three");
+        DataStream<String> elements = env.fromElements("one", "two" , "three");
 
         //data operator
+        DataStream<Integer> digits = elements.map(new TransformDigits());
 
         //data sink
-        elements.print();
+        digits.print();
 
         //如果今天sink是到別的地方database之類的就需要用到這行，用print()其實這行可以註解
         env.execute("Flink Streaming Demo");
     }
 
+    //operator
+    public static class TransformDigits implements MapFunction<String, Integer> {
+        public Integer map(String s) throws Exception {
+            if (s.equals("one"))
+                return 1;
+            else if(s.equals("two"))
+                return 2;
+            else if(s.equals("three"))
+                return 3;
+            else
+                throw new RuntimeException();
+        }
+    }
 }
